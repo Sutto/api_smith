71e70983f8ef098a1296d31efb2c9bda079dda5c
Evgeniy Kurtov <me@lessless.pp.ua>
2013-01-24 15:07:48 +0200

    Update lib/api_smith/client.rb

--- lib/api_smith/client.rb
+++ lib/api_smith/client.rb
@@ -112,7 +112,7 @@ module APISmith
         end
         # Finally, use HTTParty to get the response
         response = instrument_request method, full_path, options do
-          self.class.send method, full_path, options
+          self.class.send method, full_path, (request_options.merge options)
         end
         # Pre-process the response to check for errors.
         check_response_errors response
